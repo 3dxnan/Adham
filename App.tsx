9fe2a9ed89d280b6161451f6ad7303fbf0317c40
3dxnan <adnankhaledagha@gmail.com>
2026-02-12 13:43:26 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -652,7 +652,7 @@ const App = () => {
                 style={{ width: "100%", borderRadius: "15px" }}
               >
                 <source
-                  src="https://res.cloudinary.com/dj36afflz/video/upload/v1770574748/%D8%AA%D8%A7%D9%85%D8%B1_%D8%A7%D9%84%D8%AC%D9%8A%D8%A7%D8%B1_%D8%AA%D8%B5%D8%AF%D9%82_%D8%A7%D9%86%D8%AA_%D8%B9%D9%8A%D9%84_%D8%A8%D8%B6%D8%A7%D9%86_gnj4e3.mp4"
+                  src="https://res.cloudinary.com/dj36afflz/video/upload/v1770648987/%D9%83%D9%84_%D8%B3%D9%86%D9%87_%D9%88%D8%A3%D9%85%D9%87_%D9%85%D8%AD%D9%85%D8%AF_%D8%A8%D8%AE%D9%8A%D8%B1_%D9%88%D8%B3%D8%B9%D8%A7%D8%AF%D9%87_%D9%88%D8%A8%D8%B9%D9%88%D8%AF%D8%A7%D9%87_%D8%A7%D9%84%D8%A3%D9%8A%D8%A7%D9%85_%EF%B8%8F_%D8%B1%D9%85%D8%B6%D8%A7%D9%86_%D9%83%D8%B1%D9%8A%D9%85_%D8%AA%D9%88%D8%A7%D9%85_%D9%85%D8%B5%D8%B1_%D8%B4%D9%87%D8%A7%D8%A8_%D9%88%D8%B4%D8%A7%D9%87%D8%B1_%D8%A7%D9%84%D8%AA%D9%88%D8%A7%D9%85_%D8%A7%D9%84%D9%86%D8%A8%D8%B7%D8%B4%D9%89_oiwchn.mp4"
                   type="video/mp4"
                 />
               </video>
