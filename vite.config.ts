1df1dca008d4644553d3074a8da1d849e0757110
3dxnan <adnankhaledagha@gmail.com>
2026-02-16 15:08:09 +0200

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // السطر ده هو اللي هيخلي الصور والملفات تشتغل صح أوتوماتيك على GitHub Pages
-  base: "./",
+  // غيرنا دي لاسم المستودع بالظبط بين 2 سلاش
+  base: "/Adham/", 
   server: {
     allowedHosts: ["qkvpp4-5173.csb.app"],
   },
