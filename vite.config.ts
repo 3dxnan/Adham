8c0806fe190190647c3e380cf0701c776e720e44
3dxnan <adnankhaledagha@gmail.com>
2026-02-16 17:25:55 +0200

    Change base path for GitHub Pages
    Update Vite configuration for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // السطر ده هو اللي هيخلي الصور والملفات تشتغل صح أوتوماتيك على GitHub Pages
-  base: "./",
-  server: {
-    allowedHosts: ["qkvpp4-5173.csb.app"],
+  base: "/Adham/", // ده لازم يكون اسم المستودع بتاعك بالظبط
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
